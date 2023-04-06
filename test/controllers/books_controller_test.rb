0112608ef6df09c63c27f6a8b4e5f79973bd0d89
pete_jdv <p.alvarado@jvd.com.ph>
2023-04-06 17:57:25 +0800

    test: add test for Books API Controller

---
+++ test/controllers/books_controller_test.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class BooksControllerTest < ActionDispatch::IntegrationTest
+  test 'get book should successfully' do
+    get '/api/v1/books/978-1-891830-85-3'
+    assert_response :success
+  end
+
+  test 'get book should respond not found' do
+    get '/api/v1/books/978-1-891830-85-5'
+    assert_response :not_found
+  end
+
+  test 'convert isbn-13 to isbn-10 should successfully' do
+    get '/api/v1/books/978-1-891830-85-3/convert-isbn'
+    assert_response :success
+  end
+
+  test 'convert isbn-13 to isbn-10 should raise 400' do
+    get '/api/v1/books/979-1-891830-85-5/convert-isbn'
+    assert_response(400)
+  end
+end
