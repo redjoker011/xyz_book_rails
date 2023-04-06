1bb1c14fcaeb67b79b6ff2390a87b086bd2d16be
pete_jdv <p.alvarado@jvd.com.ph>
2023-04-06 16:11:48 +0800

    feat: create new route for converting isbn-13 to isbn-10

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -6,13 +6,14 @@ module Api
   module V1
     # Book API Controller
     class BooksController < ActionController::API
+      before_action :valid_isbn_format?
+      before_action :initialize_isbn, only: :convert_isbn
+
       # Fetch book by isbn
       #
       # @return [JSON]
       def book
         isbn = params[:id]
-
-        return response_invalid unless valid_isbn?
 
         @book = Book.find_by_isbn_thirteen(isbn)
 
@@ -21,6 +22,16 @@ module Api
         else
           response_not_found
         end
+      end
+
+      def convert_isbn
+        isbn_ten = @isbn_service.to_ten(params[:id])
+        render json: {
+          isbn_ten: format_isbn_ten(isbn_ten),
+          isbn_thirteen: params[:id]
+        }
+      rescue InvalidISBNError, Invalid13DigitISBN
+        response_invalid_format
       end
 
       private
@@ -42,15 +53,29 @@ module Api
       end
 
       # 400 HTTP Code Response
-      def response_invalid
-        render json: { error: 'id param invalid'.to_json }, status: 400
+      def response_invalid_format
+        render json: { error: 'id param is not a valid ISBN-13 Digit'.to_json }, status: 400
       end
 
       # Validate ISBN Format
       #
-      # @return [Boolean]
-      def valid_isbn?
-        params[:id].match(Book::ISBN_THIRTEEN_FORMAT).present?
+      # @return [JSON]
+      def valid_isbn_format?
+        is_valid = params[:id].match(Book::ISBN_THIRTEEN_FORMAT).present?
+
+        return response_invalid_format unless is_valid
+      end
+
+      # Initialize ISBN class
+      def initialize_isbn
+        @isbn_service = ISBN.new
+      end
+
+      # @param [String] isbn
+      #
+      # @return [String] formatted isbn
+      def format_isbn_ten(isbn)
+        isbn.gsub(/(\d{1})(\d{3})(\d{5})(\d{1})/, '\\1-\\2-\\3-\\4')
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@
 Rails.application.routes.draw do
   scope path: 'api/v1/books', module: 'api/v1', as: :v1 do
     get ':id', to: 'books#book'
+    get ':id/convert-isbn', to: 'books#convert_isbn'
   end
 end
