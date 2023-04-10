f558fa98dd051e10ff75b1b54d37c7f09969ee2b
pete_jdv <p.alvarado@jvd.com.ph>
2023-04-10 17:05:33 +0800

    feat: setup initial book page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,13 @@
 class BooksController < ApplicationController
   def index; end
 
-  def show; end
+  def show
+    isbn = params[:id]
+    @book = Book.find_by_isbn_thirteen(isbn)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @book }
+    end
+  end
 end
